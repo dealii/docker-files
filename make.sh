7dd2b8ed36ce2ddf49d82c2c9ae94b451139a542
Timo Heister <timo.heister@gmail.com>
2016-04-22 17:27:12 +0100

    update

--- make.sh
+++ make.sh
@@ -7,10 +7,11 @@ docker build -t $repo/dealii-base base/ || exit 1
 
 ver="v8.4.1"
 docker build -t $repo/dealii-bare:$ver --build-arg VER=$ver bare/ || exit 1
-ver="master"
-docker build -t $repo/dealii-bare:$ver --build-arg VER=$ver bare/ || exit 1
 
-docker build -t $repo/dealii-full-deps:v0.3 full-deps/ || exit 1
+ver="master"
+#docker build -t $repo/dealii-bare:$ver --build-arg VER=$ver bare/ || exit 1
+
+docker build -t $repo/dealii-full-deps full-deps/ || exit 1
 
 ver="v8.4.1"
 docker build -t $repo/dealii:v8.4.1 --build-arg VER=$ver deal/ || exit 1
